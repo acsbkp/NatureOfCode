5d77fd938983b45eb02c72bb7c74ad148e5d8790
AbhiroopKar-p5Creator <71604858+AbhiroopKar-p5Creator@users.noreply.github.com>
2020-09-27 13:39:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,3 @@
-// Https://github.com/AbhiroopKar-p5Creator/NatureOfCode/Constructing_Random_walker/sketch.js
-// Nature Of Code
-// Abhiroop Kar
-
 var particle;
 var choise;
 var x = 683
@@ -15,20 +11,15 @@ function draw() {
 	stroke(255,0,0)
 	point(x,y)
 
- choise = round(random(0,3)) 
+ choise = random(1)
  
- if(choise === 0){
+ if(choise < 0.4){
  	x = x + 1
- } else if (choise === 1){
+ } else if (choise < 0.6 && choise > 0.4){
  	y = y - 1
- } else if(choise === 2){
+ } else if(choise < 0.8 && choise > 0.6){
  	x = x - 1
- } else if(choise ===3) {
+ } else {
  	y = y + 1
  }
 }
-// So,this is asimplified version of the nature of code
-// this code example is obtainded by Nature of code First edition
-// It is there without OOP(ObjectOrientedProgramming)edited by me(Abhiroop Kar)
-// If you liked this code please contact me on my email (abhiroop.kar2010@gmail.com)
-// Please comment, a part of it goes to Daniel Shiffmen my inspiration.
